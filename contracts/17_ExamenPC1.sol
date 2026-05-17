0c331469a1c6015b96019ca85e848a209e7873c8
migueeg <maeg2468@gmail.com>
2026-05-16 20:03:45 -0500

    Parte 2

--- contracts/17_ExamenPC1.sol
+++ contracts/17_ExamenPC1.sol
@@ -5,14 +5,29 @@ import "hardhat/console.sol";
 
 contract SistemaInventario259967 {
     struct NombreStruct {
-    uint256 id;
-    string nombre;
-    uint256 cantidad;
-}
+        uint256 id;
+        string nombre;
+        uint256 cantidad;
+    }
 
-NombreStruct[] public registros;
+    NombreStruct[] public registros;
 
-constructor() {
-    console.log("Ejecutado por: 259967 - MIGUEL ANGEL ESCOBAR GOMEZ");
-}
+    address public dirContrato = 0x358AA13c52544ECCEF6B0ADD0f801012ADAD5eE3;
+
+    constructor() {
+        console.log("Ejecutado por: 259967 - MIGUEL ANGEL ESCOBAR GOMEZ");
+    }
+
+    function agregarElemento(
+        uint256 _id,
+        string memory _nombre,
+        uint256 _cantidad
+    ) public {
+        registros.push(NombreStruct(_id, _nombre, _cantidad));
+    }
+
+    function contarElementos() public view returns (uint256) {
+        console.log("Ejecutado por: 259967 - MIGUEL ANGEL ESCOBAR GOMEZ");
+        return registros.length;
+    }
 }
